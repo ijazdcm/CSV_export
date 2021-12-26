a1e34f4f7710f505dd45ba9d4bf40acf39c1da06
MiracleDCM <ijazdcm007@gmail.com>
2021-12-27 01:30:47 +0530

    Final

--- resources/views/file-import.blade.php
+++ resources/views/file-import.blade.php
@@ -10,21 +10,6 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <!-- Links
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-          </ul>-->
-          <!-- Navbar text-->
-          <span class="navbar-text">
-            Admin
-          </span>
-      </nav>
     <div class="container mt-5 text-center">
         <h1 class="mb-4">
             Export Report
@@ -33,7 +18,6 @@
         <form action="{{ route('file-import')}}" method="POST" enctype="multipart/form-data">
             @csrf
             <a class="btn btn-success" href="{{ route('file-export') }}">Export as CSV</a>
-            {{--<a class="btn btn-success" href="{{ route('file-export') }}">Export as XLS</a>--}}
 
         </form>
     </div>
